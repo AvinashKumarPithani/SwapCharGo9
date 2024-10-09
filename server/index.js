34cf81334290faf958e02cac5193c048ce2cfbce
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-10-09 08:25:49 +0530

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import roomRouter from "./routes/roomRouter.js";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,18 +11,24 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, Content-Type, Authorization"
-  );
-  next();
-});
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Allow requests from your client URL
+  methods: "GET, POST, PUT, PATCH, DELETE", // Allowed methods
+  allowedHeaders: "X-Requested-With, Content-Type, Authorization" // Allowed headers
+}));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, Content-Type, Authorization"
+//   );
+//   next();
+// });
 
 app.use(express.json({ limit: "10mb" }));
 app.use("/user", userRouter);
