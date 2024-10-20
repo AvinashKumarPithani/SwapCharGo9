665f91ebf7312519289ced0d7c799a689199a4ae
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-10-20 22:04:18 +0530

    updated the code format

--- swap-charge-go/src/App.js
+++ swap-charge-go/src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import ContextProvider from "./context/ContextProvider";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />}></Route>
-        <Route path="/map" element={
-        <ContextProvider><Map /></ContextProvider>}
+        <Route
+          path="/map"
+          element={
+            <ContextProvider>
+              <Map />
+            </ContextProvider>
+          }
         ></Route>
       </Routes>
     </BrowserRouter>
